20123d82c32069654adf2861dfa327635c5d79da
Torycd <vicidundun@gmail.com>
2024-09-03 23:58:34 +0100

    okay

--- src/MainLandingPage.jsx
+++ src/MainLandingPage.jsx
@@ -4,7 +4,7 @@ import NavigationMenu from "./components/Navigation/NavigationMenu";
 const MainLandingPage = () => {
   return (
     <div>
-      <div className="lg:px-64 md:px-24 sm:px-10 px-4 pt-10">
+      <div className="lg:px-64 md:px-24 sm:px-10 px-4">
         <NavigationMenu />
         <Outlet />
       </div>
--- src/components/Navigation/NavigationMenu.jsx
+++ src/components/Navigation/NavigationMenu.jsx
@@ -13,11 +13,11 @@ const NavigationMenu = () => {
   return (
     <div className="mb-20">
       <div className="flex justify-between">
-        <div className="font-bold text-xl flex items-center gap-1">
+        <div className="font-bold text-xl flex items-center gap-1 mt-10">
           <div className="bg-blue-800 text-white p-1 rounded-md">Pp</div>
           <h2 className="">Pen & Pixel</h2>
         </div>
-        <div className="hidden md:flex gap-4 text-gray-700">
+        <div className="hidden md:flex gap-4 text-gray-700 mt-10">
           <ul className="flex items-center gap-2">
             <li className={navClass}>About</li>
             <li className={navClass}>Blog</li>
@@ -28,20 +28,21 @@ const NavigationMenu = () => {
           <Button>Sign Up</Button>
         </div>
         {/* mobile menu */}
-        <div className={nav ? "md:hidden fixed right-0 px-2  w-full" : ""}>
+        <div className={nav ? "md:hidden fixed right-0 mt-5 px-2  w-full" : ""}>
           <div
             className={
               nav
-                ? "w-[100%] border-gray-300 border p-10 z-50  bg-white  text-black ease-in duration-500"
-                : "fixed right-[-200%]  border-gray-300 border p-10  ease-in duration-800"
+                ? "fixed right-0 w-[100%] border-gray-300 rounded-md border p-10 z-50  bg-white  text-gray-700 ease-in duration-500"
+                : "fixed right-[-100%] z-50 rounded-md  border-gray-300 border p-10  ease-in duration-800"
             }
           >
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col text-xl font-medium items-center gap-4">
               <ul className="text-center space-y-5">
                 <li>About</li>
                 <li>Blog</li>
                 <li>Pricing</li>
                 <li>Contact Us</li>
+                <li>Login</li>
               </ul>
               <Button>Sign Up</Button>
             </div>
@@ -49,7 +50,7 @@ const NavigationMenu = () => {
         </div>
         <div
           onClick={handleNav}
-          className=" text-blue-800 flex justify-center z-50 items-center md:hidden relative"
+          className=" text-blue-800 flex justify-center mt-10 z-50 items-center md:hidden relative"
         >
           {!nav ? <AiOutlineMenu size={25} /> : <AiOutlineClose size={25} />}
         </div>
