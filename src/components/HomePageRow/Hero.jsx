ec9764413a1807b77afa247b61a80b61b90a4a9e
Torycd <vicidundun@gmail.com>
2024-09-03 21:47:27 +0100

    lets go

---
+++ src/components/HomePageRow/Hero.jsx
@@ -0,0 +1,28 @@
+// import React from 'react'
+
+import Button from "../Button/Button";
+
+const Hero = () => {
+  return (
+    <div className="flex items-center">
+      <div className="space-y-4">
+        <h2 className="text-blue-800 uppercase font-medium">
+          about Pen & Pixel
+        </h2>
+        <div className="text-black font-bold text-6xl space-y-2">
+          <h2>We are a modern</h2>
+          <h2>design agency.</h2>
+        </div>
+        <p className="font-medium text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <Button>See Portfolio</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 // import React from 'react'
+import Hero from "../components/HomePageRow/Hero";
 import image from "../images/heoImg.jpeg";
 
 const HomePage = () => {
   return (
-    <div className="grid grid-cols-2">
-      <div></div>
-
+    <div className="grid grid-cols-2 md:gap-20">
+      <Hero/>
       <img className="rounded-lg" src={image} alt="Hero" />
     </div>
   );
