1f2e08337ccb49176ff33e4318992e2a8800171d
Torycd <vicidundun@gmail.com>
2024-09-03 23:47:33 +0100

    navigation almost through

--- src/components/Navigation/NavigationMenu.jsx
+++ src/components/Navigation/NavigationMenu.jsx
@@ -1,24 +1,57 @@
-// import React from "react";
+import { useState } from "react";
+
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import Button from "../Button/Button";
 
 const NavigationMenu = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
   const navClass =
     "transition duration-300 font-medium mr-5 border-b-2 border-white   hover:border-gray-700";
   return (
-    <div className="sm:mb-10">
+    <div className="mb-20">
       <div className="flex justify-between">
-        <div>
-          <h2 className="font-bold text-4xl">Pen & Pixel</h2>
+        <div className="font-bold text-xl flex items-center gap-1">
+          <div className="bg-blue-800 text-white p-1 rounded-md">Pp</div>
+          <h2 className="">Pen & Pixel</h2>
         </div>
-        <div className="flex gap-4 text-gray-700">
-          <ul className="flex items-center gap-2 ">
+        <div className="hidden md:flex gap-4 text-gray-700">
+          <ul className="flex items-center gap-2">
             <li className={navClass}>About</li>
             <li className={navClass}>Blog</li>
             <li className={navClass}>Pricing</li>
             <li className={navClass}>Contact Us</li>
             <li className={`ml-16 ${navClass}`}>Login</li>
           </ul>
-          <Button>sIgn up</Button>
+          <Button>Sign Up</Button>
+        </div>
+        {/* mobile menu */}
+        <div className={nav ? "md:hidden fixed right-0 px-2  w-full" : ""}>
+          <div
+            className={
+              nav
+                ? "w-[100%] border-gray-300 border p-10 z-50  bg-white  text-black ease-in duration-500"
+                : "fixed right-[-200%]  border-gray-300 border p-10  ease-in duration-800"
+            }
+          >
+            <div className="flex flex-col items-center">
+              <ul className="text-center space-y-5">
+                <li>About</li>
+                <li>Blog</li>
+                <li>Pricing</li>
+                <li>Contact Us</li>
+              </ul>
+              <Button>Sign Up</Button>
+            </div>
+          </div>
+        </div>
+        <div
+          onClick={handleNav}
+          className=" text-blue-800 flex justify-center z-50 items-center md:hidden relative"
+        >
+          {!nav ? <AiOutlineMenu size={25} /> : <AiOutlineClose size={25} />}
         </div>
       </div>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,9 @@ import image from "../images/heoImg.jpeg";
 
 const HomePage = () => {
   return (
-    <div className="grid grid-cols-2 md:gap-20">
+    <div className="grid gap-10 sm:grid-cols-2 grid-cols-1 md:gap-20 ">
       <Hero/>
-      <img className="rounded-lg" src={image} alt="Hero" />
+      <img className="rounded-lg order-first sm:order-last" src={image} alt="Hero" />
     </div>
   );
 };
