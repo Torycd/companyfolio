8d7984c9433b7c485ed3a056097d695eaf8e67ff
Torycd <vicidundun@gmail.com>
2024-09-06 09:58:33 +0100

    Animation Added

---
+++ src/components/Helpers/Animation.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { motion } from "framer-motion";
+import useInView from "../Helpers/UseInView";
+
+function AnimationReveal({ disabled, children }) {
+  if (disabled) {
+    return <>{children}</>;
+  }
+
+  const directions = ["left", "right"];
+  
+  const childrenWithAnimation = React.Children.toArray(children).map((child, i) => (
+    <AnimatedSlideInComponent
+      key={i}
+      direction={directions[i % directions.length]}
+    >
+      {child}
+    </AnimatedSlideInComponent>
+  ));
+
+  return <>{childrenWithAnimation}</>;
+}
+
+function AnimatedSlideInComponent({ direction = "left", offset = 30, children }) {
+  const [ref, inView] = useInView({ margin: `-${offset}px 0px 0px 0px` });
+
+  const initialX = direction === "left" ? "-150%" : "150%";
+
+  return (
+    <div ref={ref}>
+      <motion.section
+        initial={{ x: initialX }}
+        animate={{ 
+          x: inView ? "0%" : initialX,
+          transitionEnd: { x: inView ? 0 : initialX }
+        }}
+        transition={{ type: "spring", damping: 19 }}
+      >
+        {children}
+      </motion.section>
+    </div>
+  );
+}
+
+const MyComponent = (props) => (
+  <>
+    <AnimationReveal {...props} />
+  </>
+);
+
+export default MyComponent;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-
+import Animation from "../components/Helpers/Animation"
 import Hero from "../components/HomePageRow/Hero";
 import Values from "../components/HomePageRow/Values";
 import Vision from "../components/HomePageRow/Vision";
@@ -8,11 +8,11 @@ import Vision from "../components/HomePageRow/Vision";
 const HomePage = () => {
  
   return (
-    <div className="space-y-10">
+    <Animation>
       <Hero/>
       <Vision />
       <Values/>
-    </div>
+    </Animation>
   );
 };
 
