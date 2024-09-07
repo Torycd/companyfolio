15684d2445ed57e34079808ed55eb7d98ac49151
Torycd <vicidundun@gmail.com>
2024-09-07 18:27:02 +0100

    lets go

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
   const footNav =
     "transition ease-in-out duration-500 underline-offset-4 text-gray-500 hover:underline font-medium";
   return (
-    <div className="mt-20 bg-blue-100 lg:px-64 md:px-24 sm:px-10 px-4 py-20 text-gray-500">
+    <div className="mt-20 bg-gray-200 lg:px-64 md:px-24 sm:px-10 px-4 py-20 text-gray-500">
       <div className="flex flex-col md:flex-row md:justify-between md:gap-10">
-        <div className="flex flex-wrap justify-center items-center text-center md:text-left  gap-5 md:gap-20 text-gray-700">
+        <div className="grid grid-cols-3 md:grid-cols-4 gap-5 md:gap-20 text-gray-700  text-center md:text-left">
           <div>
             <h2 className={footHeder}>Main</h2>
             <ul className="flex flex-col gap-2">
@@ -15,7 +15,7 @@ const Footer = () => {
               <li className={footNav}>FAQs</li>
               <li className={footNav}>Support</li>
               <li className={footNav}>About Us</li>
-            </ul> 
+            </ul>
           </div>
           <div>
             <h2 className={footHeder}>Product</h2>
@@ -35,7 +35,7 @@ const Footer = () => {
               <li className={footNav}>Office</li>
             </ul>
           </div>
-          <div className="self-center">
+          <div className="col-span-3 mb-5 md:mb-0 md:col-span-1 justify-self-center">
             <h2 className={footHeder}>Legal</h2>
             <ul className="flex flex-col gap-2">
               <li className={footNav}>GDPR</li>
@@ -45,6 +45,7 @@ const Footer = () => {
             </ul>
           </div>
         </div>
+
         <div className="">
           <h2 className={`text-gray-700 ${footHeder}`}>
             Subscribe to our Newsletter
@@ -53,12 +54,12 @@ const Footer = () => {
             We deliver high quality blog posts written by<br></br> professionals
             weekly. And we promise no spam.
           </p>
-          <div className="w-full h-12 flex">
+          <div className="w-full h-28 md:h-12 flex flex-col md:flex-row gap-4 md:gap-0 ">
             <input
               placeholder="Your Email Address"
-              className="h-full w-[65%] px-4 border border-gray-300 rounded-tl-lg rounded-bl-lg focus:outline focus:outline-blue-700 focus:outline-2"
+              className="h-full w-100 md:w-[65%] px-4 border placeholder-opacity-55 bg-gray-200  border-gray-300 rounded-lg md:rounded-none md:rounded-tl-lg md:rounded-bl-lg  focus:outline focus:outline-blue-700 focus:outline-2"
             />
-            <button className="h-full w-[35%] rounded-br-lg rounded-tr-lg text-white bg-blue-700 hover:bg-blue-800 transition duration-300">
+            <button className="h-full w-100 md:w-[35%] rounded-lg md:rounded-none md:rounded-br-lg md:rounded-tr-lg text-white bg-blue-700 hover:bg-blue-800 transition duration-300">
               Subscribe
             </button>
           </div>
