253b3297bca4e39e393d6fb9bac12d3d1caf59d7
Torycd <vicidundun@gmail.com>
2024-09-08 21:10:09 +0100

    Blog page update

--- src/MainLandingPage.jsx
+++ src/MainLandingPage.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer/Footer";
 
 const MainLandingPage = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-between">
+    <div className="min-h-screen flex flex-col justify-between font-Oswald">
       <div className="fixed top-0 left-0 right-0 xl:px-64  sm:px-10 px-4 py-8  z-50 bg-white">
         <NavigationMenu />
       </div>
--- src/components/BlogPageRow/Intro.jsx
+++ src/components/BlogPageRow/Intro.jsx
@@ -1,9 +1,21 @@
 // import React from 'react'
+import intro from "../../images/Intro.jpg";
 
 const Intro = () => {
   return (
-    <div className="">Intro</div>
-  )
-}
+    <div className="mt-40">
+      <h2 className="text-6xl my-20 font-bold">Blog Post</h2>
+      <div className="flex flex-row bg-gray-100 rounded-lg">
+        <img className="max-h-96 rounded-bl-lg" src={intro} alt="vlog" />
+        <div className="flex flex-col items-center p-8 md:items-start">
+          <h2 className="uppercase text-blue-800 mb-5 text-xs font-bold tracking-widest leading-loose after:content after:block after:border-b-2 after:border-blue-700 after:w-8">
+            Design Tips
+          </h2>
+          <p className="uppercase font-bold text-gray-600 italic">September 8, 2024</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Intro
+export default Intro;
