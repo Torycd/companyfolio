6e4e3b2d5ccfc1f2283690a2993024237ef05a32
Torycd <vicidundun@gmail.com>
2024-09-08 10:38:35 +0100

    lets go

--- src/components/Footer/FooterButtom.jsx
+++ src/components/Footer/FooterButtom.jsx
@@ -6,10 +6,10 @@ const FooterButtom = () => {
     <div className="flex flex-col items-center gap-5 md:gap-0 md:flex-row justify-between border-t">
       <div className="font-bold text-xl flex items-center gap-1">
         <div className="bg-blue-800 text-white p-1 rounded-md">Pp</div>
-        <h2 className="">Pen & Pixel</h2>
+        <h2 className="">Pen & Pixel Inc.</h2>
       </div>
       <div>
-        <p>© 2018 Treact Inc. All Rights Reserved.</p>
+        <p>© 2024 Pen & Pixel Inc. All Rights Reserved.</p>
       </div>
       <div className="flex justify-between  gap-5">
         <a href="/">
