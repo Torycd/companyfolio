8c451718a2a2ce28f9196a5548adb4a7e8327441
Torycd <vicidundun@gmail.com>
2024-09-14 13:35:32 +0100

    okay

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@
 
 const  Button = ({children, props }) => {
   return (
-    <button className="bg-blue-800 hover:bg-blue-700 text-white rounded-md px-5 py-2" {...props}>{children}</button>
+    <button className="bg-blue-800 hover:bg-blue-700 text-white rounded-md px-6 py-3" {...props}>{children}</button>
   )
 }
 
--- src/components/HomePageRow/Hero.jsx
+++ src/components/HomePageRow/Hero.jsx
@@ -1,6 +1,8 @@
 // import React from 'react'
+import { FaRegPlayCircle } from "react-icons/fa";
 import image from "../../images/heoImg.jpeg";
-// import Detail from "../Card/Detail";
+import Button from "../Button/Button";
+
 
 const Hero = () => {
   // const btn = "Contact Us";
@@ -10,10 +12,25 @@ const Hero = () => {
   //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   return (
     <div className="grid md:grid-cols-2 h-screen grid-cols-1 md:gap-20 ">
-      <div className="flex items-center text-center md:text-left">
-        <div className="text-gray-800 font-bold text-xl xl:text-5xl">
-          <div>Beautiful & Cheap</div>
-          <div className="bg-blue-800 py-2 text-white transform -skew-x-12 inline-block">Simple & Classic.</div>
+      <div className="flex flex-col justify-center space-y-4">
+        <div className="text-gray-800 items-center md:text-left z-10 text-center text-nowrap font-bold leading-snug text-xl xl:text-6xl">
+          <h2>Beautiful & Cheap</h2>
+          <h2 className="bg-blue-800 p-2 mt-4 text-white transform -skew-x-12 inline-block z-100">
+            Simple & Classic.
+          </h2>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div className="flex">
+          <Button>OrderNow</Button>
+          <div className="flex font-medium text-gray-500 transition-all duration-500 hover:text-blue-400">
+            <div className="items-center flex px-4">
+              <FaRegPlayCircle  size={35} />
+            </div>
+            <p className="text-center justify-center flex flex-col" >Meet the Designers</p>
+          </div>
         </div>
       </div>
       <img
