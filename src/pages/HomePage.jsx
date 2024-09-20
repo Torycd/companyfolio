9411c38e0cc2f1da2f93ee183458e70389eb612d
Torycd <vicidundun@gmail.com>
2024-09-20 09:01:49 +0100

    okay

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@
 import Animation from "../components/Helpers/Animation"
 import Hero from "../components/HomePageRow/Hero"
 import Info from "../components/HomePageRow/Info"
+import Menu from "../components/HomePageRow/Menu"
 
 const HomePage = () => {
   return (
     <Animation>
         <Hero/>
         <Info/>
+        <Menu/>
     </Animation>
   )
 }
