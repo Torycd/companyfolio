422ed750892f1f8c7098a76efcac47d63400cb45
Torycd <vicidundun@gmail.com>
2024-09-23 07:28:04 +0100

    hell yeah

--- src/components/HomePageRow/Menu.jsx
+++ src/components/HomePageRow/Menu.jsx
@@ -3,12 +3,12 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const Menu = () => {
   const activeClass =
-    " bg-blue-800 hover:bg-blue-700 text-white rounded-md px-6 py-3";
-  const navClass = " p-2 ";
+    " bg-blue-800 hover:bg-blue-700 text-white  px-6 py-2";
+  const navClass = "px-6 py-2 ";
   return (
     <div className="min-h-screen my-24">
       <div className="flex justify-between">
-        <div className="flex text-gray-800 items-center md:text-left z-10 text-center text-nowrap font-bold leading-snug text-3xl xl:text-5xl">
+        <div className="flex text-gray-800 mb-10 items-center md:text-left z-10 text-center text-nowrap font-bold leading-snug text-3xl xl:text-5xl">
           <h2>Checkout our</h2>
           <h2 className="ml-5 bg-blue-800 p-2 mt-4 text-white transform -skew-x-12 inline-block z-100">
             Designs.
